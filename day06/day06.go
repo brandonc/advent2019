04a51258a67bdb68435546c2d51c58b2be663383
Brandon Croft <brandon.croft@gmail.com>
2019-12-06 00:00:22 -0700

    day 6 part 2

--- day06/day06.go
+++ day06/day06.go
@@ -8,13 +8,26 @@ import (
 	"github.com/brandonc/advent2019/tools"
 )
 
-func distancefromcom(orbits map[string]string, thing string) int {
+func distancefromCOM(orbits map[string]string, thing string) int {
 	if thing == "COM" {
 		return 0
 	}
 
-	return 1 + distancefromcom(orbits, orbits[thing])
+	return 1 + distancefromCOM(orbits, orbits[thing])
+}
 
+func heirarchy(orbits map[string]string, start string) []string {
+	result := make([]string, 0)
+	parent := orbits[start]
+	for {
+		result = append(result, parent)
+		if parent == "COM" {
+			break
+		}
+		parent = orbits[parent]
+	}
+
+	return result
 }
 
 func Run() {
@@ -33,8 +46,26 @@ func Run() {
 
 	total := 0
 	for id := range orbitlist {
-		total += distancefromcom(orbitlist, id)
+		total += distancefromCOM(orbitlist, id)
 	}
 
 	fmt.Println("Part 1 answer:", total)
+
+	myheirarchy := heirarchy(orbitlist, "YOU")
+	santasheirarchy := heirarchy(orbitlist, "SAN")
+
+	// Finds the orbital parent that SANTA and YOU have in common
+	transfers := 0
+	santatransfers := 0
+	for myparent := range myheirarchy {
+		santatransfers = 0
+		for santasparent := range santasheirarchy {
+			if santasheirarchy[santasparent] == myheirarchy[myparent] {
+				fmt.Println("Part 2 answer:", transfers+santatransfers)
+				os.Exit(0)
+			}
+			santatransfers += 1
+		}
+		transfers += 1
+	}
 }
